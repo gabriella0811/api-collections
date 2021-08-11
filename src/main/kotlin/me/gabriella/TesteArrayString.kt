6ebbdd1622f91862e95b3a5e6bd7d50c171896a7
Gabriella dos Reis da Trindade <gabriellareis.trindade@gmail.com>
2021-08-10 21:12:11 -0300

    Test Array String

---
+++ src/main/kotlin/me/gabriella/TesteArrayString.kt
@@ -0,0 +1,34 @@
+package me.gabriella
+
+import sun.security.krb5.internal.KDCOptions.with
+
+fun main(){
+    val size = 3
+    val nomes = Array<String>(size: 3){""}
+
+    nomes[0] = "Gabriella"
+    nomes[1] = "Mariana"
+    nomes[2] = "João"
+
+    for (nome in nomes){
+        println(nome)
+    }
+
+        for (nome in nomes){
+            println(nome)
+    }
+
+        sort()
+    }
+        nomes.forEach{
+        val it = ""
+        println(it)
+    }
+
+    val nomes2 = arrayOf("Gabriella" , "Mariana" , "João")
+        nomes2.sort()
+        nomes2.forEach{
+            val it = ""
+            println(it)
+    }
+}
