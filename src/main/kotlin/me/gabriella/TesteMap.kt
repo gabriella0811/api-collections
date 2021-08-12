43c46daa5d0b24e230d5f4f17e243b85d6064b71
Gabriella dos Reis da Trindade <gabriellareis.trindade@gmail.com>
2021-08-11 22:20:14 -0300

    Test Map

---
+++ src/main/kotlin/me/gabriella/TesteMap.kt
@@ -0,0 +1,22 @@
+package me.gabriella
+
+fun main() {
+
+    val pair: Pair<String, Double> = Pair("Gabriella", 1000.0)
+    val map1 = mapOf(pair)
+
+    map1.forEach {
+        k, v ->
+        println("Chave: $k = Valor: $v")
+    }
+
+    val map2 = mapOf(
+        "Ellen" to 2600.0,
+        "Michele" to 3000.0
+    )
+
+    map2.forEach {
+        k, v ->
+        println("Chave: $k = Valor: $v")
+    }
+}
